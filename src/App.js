649980c41eff1577f57aa0cdf7c23b6fe795dfbe
esitorsunil <sunilrio23@gmail.com>
2025-01-28 23:13:22 +0530

    route handler (app.use)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ const express = require("express")
 
 const app = express()
 
-app.use("/", (req, res) => {
-    res.send("This is my dashboard")
-})
-
 app.use("/test", (req, res) => {
     res.send("Hello from the server")
+})
+
+app.use("/", (req, res) => {
+    res.send("This is my dashboard")
 })
 
 app.listen(3000, () => {
