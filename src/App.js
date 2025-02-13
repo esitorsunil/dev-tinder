f849ed9e511899c2ec58950628128e8269e536f0
esitorsunil <sunilrio23@gmail.com>
2025-02-14 00:56:38 +0530

    handling multiple routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user", (req, res) => {
-    res.send({firstName: "Sunil", lastName: "Ferno"})
-})
 
-app.post("/user", (req, res) => {
-    res.send("User connected to the DB")
-})
 
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully")
-})
-
-app.use("/", (req, res) => {
-    res.send("This is my dashboard")
+app.use("/user", [(req, res, next) => {
+   console.log("This is my dashboard")
+   //res.send("Hello");
+   next();
+}, (req, res, next) => {
+   // res.send("2nd response")
+    next();
+}], (req, res) => {
+    res.send("3rd response")
+}, (req, res) => {
+    res.send("4th response")
 })
 
 app.listen(3000, () => {
