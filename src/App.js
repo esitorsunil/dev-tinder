b6bc675fbc4fd9b644eb186dd1c70b6e1a8fdfd0
esitorsunil <sunilrio23@gmail.com>
2025-03-03 13:53:31 +0530

    create post/API and save to DB

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const connectDB = require("./config/database")
 const app = express()
+const User = require("./models/user");
+
+
+app.post("/signup", async (req, res) => {
+    const user = new User ({
+        firstName: "Sunil",
+        lastName: "Rio",
+        emailId: "sunilrio23@gmail.com",
+        password: "sunil@123",
+    });
+
+    await user.save();
+    res.send("User has been saved");
+});
 
 connectDB().then(() => {
     console.log("Database has been established");
