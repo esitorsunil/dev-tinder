2bf12eac074815b7ee488a04e2fa7882d1cf74ac
esitorsunil <sunilrio23@gmail.com>
2025-03-04 13:19:01 +0530

    patch API

--- src/App.js
+++ src/App.js
@@ -40,9 +40,25 @@ app.delete("/user", async(req, res) => {
     const userId = req.body.userId;
 
     try {
-        const user = User.findByIdAndDelete(userId);
+        const user = await User.findByIdAndDelete(userId);
 
         res.send("User deleted succesfully");
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+//update API
+app.patch("/user", async(req, res) => {
+    const userId = req.body.userId;
+    const data = req.body
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, data, {
+            returnDocument: "after"
+        });
+
+        res.send("User updated succesfully");
     } catch (error) {
         res.status(400).send("Something went wrong");
     }
