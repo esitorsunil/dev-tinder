9d2fa6f176a813157fd24ac19af9b4dca2658fe2
esitorsunil <sunilrio23@gmail.com>
2025-03-15 12:42:48 +0530

    (accept, reject) API& its validation

--- src/routes/request.js
+++ src/routes/request.js
@@ -52,3 +52,32 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req, res) 
 })
 
 module.exports = requestRouter;
+
+requestRouter.post("/request/review/:status/:requestId", userAuth, async(req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const {status, requestId} = req.params;
+
+        const allowedStatus = ["accepted", "rejected"];
+
+        if(!allowedStatus.includes(status)) {
+            return res.status(400).json({message: "Invalid Status type: "+ status});
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested"
+        });
+        if(!connectionRequest) {
+            return res.status(400).json({message: "Request not found"});
+        }
+
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+
+        res.json ({ message: "Connection request" + status, data });
+    } catch (err) {
+        res.status(400).send("ERROR :" + err.message);
+    }    
+})
