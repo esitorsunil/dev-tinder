09da2d3be937e26005d9e75df47d8adb60ece68e
esitorsunil <sunilrio23@gmail.com>
2026-01-27 22:01:29 +0530

    secure roomid using crypto, hash

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io")
+const crypto = require("crypto")
+
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+}
 
 const initializeSocket = (server) => {
    const io = socket(server, {
@@ -9,7 +17,7 @@ const initializeSocket = (server) => {
 
    io.on("connection", (socket) => {
     socket.on("joinChat", ({firstName, userId, targetUserId}) => {
-        const roomId = [userId, targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId, targetUserId);
 
 
         console.log(firstName + " Joining room: " + roomId);
@@ -17,7 +25,7 @@ const initializeSocket = (server) => {
     });
 
     socket.on("sendMessage", ({firstName, userId, targetUserId, text}) => {
-        const roomId = [userId, targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId, targetUserId);
         console.log(firstName+ " " + text);
         io.to(roomId).emit("messageReceived", {firstName, text})
     });
